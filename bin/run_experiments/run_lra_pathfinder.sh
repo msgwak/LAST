ffedc8c1d6b53b2aa26ddae89b0b0fa22752fa88
Minseon Gwak <minseon25@postech.ac.kr>
2024-07-22 10:19:36 +0000

    pruning_epoch added

--- bin/run_experiments/run_lra_pathfinder.sh
+++ bin/run_experiments/run_lra_pathfinder.sh
@@ -1,6 +1,7 @@
 python run_train.py --pruning=True --pruning_ratio=30 \
+                    --pruning_epoch=1 --epochs=10 \
                     --C_init=trunc_standard_normal --batchnorm=True --bidirectional=True \
                     --blocks=8 --bn_momentum=0.9 --bsz=64 --d_model=192 \
-                    --dataset=pathfinder-classification  --epochs=10 --jax_seed=8180844 --lr_factor=5 \
+                    --dataset=pathfinder-classification --jax_seed=8180844 --lr_factor=5 \
                     --n_layers=6 --opt_config=standard --p_dropout=0.05 --ssm_lr_base=0.0009 \
                     --ssm_size_base=256 --warmup_end=1 --weight_decay=0.03
--- bin/run_experiments/run_mnist.sh
+++ bin/run_experiments/run_mnist.sh
@@ -1,7 +1,8 @@
 python run_train.py --pruning=True --pruning_ratio=50 \
+                    --pruning_epoch=2 --epochs=5 \
                     --dataset=mnist-classification \
                     --n_layers=4 --d_model=96 --ssm_size_base=128 --blocks=1 \
                     --p_dropout=0.1 --lr_factor=4 --ssm_lr_base=0.002 \
-                    --bsz=50 --epochs=10 --weight_decay=0.01 \
+                    --bsz=50 --weight_decay=0.01 \
                     --jax_seed=2024 \
                     # --USE_WANDB=True --wandb_project=s5
